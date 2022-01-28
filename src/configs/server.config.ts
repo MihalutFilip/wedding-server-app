4b9efe3f0d89ee594e9d8f60f9a157501b1d4569
mihalutfilip <filipmihalut@gmail.com>
2022-01-28 17:04:48 +0200

    Update process env port

--- src/configs/server.config.ts
+++ src/configs/server.config.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 export const serverConfig: Record<string, string | number> = {
   API_BASE_PATH: process.env.API_BASE_PATH || '/api',
-  PORT: parseInt(process.env.API_PORT as string, 10) || 80,
+  PORT: process.env.PORT || 80,
   DATABASE_URI: process.env.MONGODB_URI || 'mongodb://localhost/wedding-app'
 };
